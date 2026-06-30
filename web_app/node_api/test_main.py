4ad287e98f71769c8906bb0cbd12482c3097e430
523h0028@student.tdtu.edu.vn <hieuduong2509@gmail.com>
2026-06-30 16:45:50 +0700

    Feat: Unit Test

---
+++ web_app/node_api/test_main.py
@@ -0,0 +1,20 @@
+from fastapi.testClient import TestClient
+from main import app
+
+client = TestClient(app)
+def test_get_jobs():
+    expected_status = 200
+    expected_response = {"success":true,"data":[{"id":1,"title":"DevOps Engineer","status":"Open"},{"id":2,"title":"Backend Developer","status":"Closed"}]}
+
+    response = client.get("/api/jobs")
+    assert response.status_code == expected_status
+    assert response.json() == expected_response
+
+def test_create_job():
+    pass
+
+def test_update_job():
+    pass
+
+def test_delete_job():
+    pass
