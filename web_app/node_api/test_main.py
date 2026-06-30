a13abe1d279087a1289c0d4d9fef486b26ab2205
523h0028@student.tdtu.edu.vn <hieuduong2509@gmail.com>
2026-06-30 17:36:21 +0700

    Fix: Change boolean 'true' to 'True' in Python test case

--- web_app/node_api/test_main.py
+++ web_app/node_api/test_main.py
@@ -4,7 +4,7 @@ from main import app
 client = TestClient(app)
 def test_get_jobs():
     expected_status = 200
-    expected_response = {"success":true,"data":[{"id":1,"title":"DevOps Engineer","status":"Open"},{"id":2,"title":"Backend Developer","status":"Closed"}]}
+    expected_response = {"success":True,"data":[{"id":1,"title":"DevOps Engineer","status":"Open"},{"id":2,"title":"Backend Developer","status":"Closed"}]}
 
     response = client.get("/api/jobs")
     assert response.status_code == expected_status
